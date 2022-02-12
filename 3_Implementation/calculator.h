6867454de6838b744135f41290e66caf4d84082e
shyam sundar <99126857+shyamsundar1682@users.noreply.github.com>
2022-02-12 10:15:07 +0530

    Add files via upload

---
+++ 3_Implementation/calculator.h
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+int addition(int a, int b)
+{
+ return (a+b); 
+}
+int subtraction(int a, int b)
+{ 
+    return (a-b); 
+} 
+int multiplication(int a,int b)
+{
+    return (a*b); 
+}
+int division(int n1, int n2)
+{
+    
+    return (n1/n2);
+}
+int modulus(int a,int b)
+{
+    
+   return (a%b);
+    
+}
